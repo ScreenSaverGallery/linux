a09fc4966ef3d7f8b8d23aacac3d6dbe3fd73d38
Tomáš <info@metazoa.org>
2024-07-17 00:18:57 +0200

    linux setup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Tray, Menu } from 'electron';
 import * as remote from '@electron/remote/main';
 import { ipcMain } from 'electron';
 import { Store } from './app/store';
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid'; // module not found error
 import * as path from 'path';
 import * as ChildProcess from 'child_process';
 
-// 🐶 TODO: add Tray (see: https://electronjs.org/docs/latest/api/tray)
+// 🐶 TODO: TIMING, POWER, UPDATER
 
 declare const CONFIG_WINDOW_WEBPACK_ENTRY: string;
 declare const CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
@@ -25,128 +25,102 @@ remote.initialize();
 app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 
 // updater
-const updater = new Updater();
-updater.check();
+// const updater = new Updater();
+// updater.check();
+
+// tray
+let tray!: Tray;
 
 // app user data storage
-const storeDefaults: any = {
-	devMode: false,
-	muted: false,
-	sensitive: false,
-	voiceOver: false,
-	id: undefined
-}
 const store: Store = new Store({
 	userDataPath: app.getPath('userData'),
 	configName: 'config',
-	defaults: storeDefaults
 });
 // remote window – ssg options
 const remoteWindow = ipcMain;
-// devMode
-let devMode = store.getDevMode;
-const sensitive = store.getSensitive;
-const muted = store.getMuted;
-const voiceOver = store.getVoiceOver;
-if (!store.getId) store.setId = uuidv4(); // set id for navigator if not set
+
+
+if (!store.id) store.id = uuidv4(); // set id for navigator if not set
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-	app.quit();
+	// app.quit();
 });
 
-app.on("ready", () => {
-	console.log('argv', process.argv); 
-	console.log('app.isPacked?', app.isPackaged);
-	
-
-	remoteWindow.on('message', (event: any, data: any) => {
+app.on('ready', () => {
+    // tray
+    initTray();
+    // config/remote window sent message
+    remoteWindow.on('message', (event: any, data: any) => {
 		console.log('data', data);
-		if (data.devMode) store.setDevMode = data.value;
-		if (data.sensitive) store.setSensitive = data.value;
-		if (data.muted) store.setMuted = data.value;
+		if (data.devMode) store.devMode = data.value;
+		if (data.sensitive) store.sensitive = data.value;
+		if (data.muted) store.muted = data.value;
+        if (data.voiceOver) store.voiceOver = data.value;
+        if (data.startSSG) store.startSSG = data.value;
+        if (data.displayOff) store.displayOff = data.value;
+        if (data.requirePass) store.requirePass = data.value;
+        if (data.runOnBattery) store.runOnBattery = data.value;
 	});
-	
-	const argNum = 1; // in production it is 1, for dev set 2
-	// console.log('CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY', CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY);
-	// console.log('CONFIG_WINDOW_WEBPACK_ENTRY', CONFIG_WINDOW_WEBPACK_ENTRY);
-	if(process.argv.length > 1) {
-		/* OPTIONS DIALOG (MODAL) */
-		if ( process.argv[argNum] === "/C" || process.argv[argNum].match(/^\/c/)) { // configuration
-			const modal = new BrowserWindow({
-				width: 700, 
-				height: 400, 
-				webPreferences: {
-					sandbox: false,
-					nodeIntegration: true,
-					preload: CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY, // `${__dirname}/assets/modal/modal.js`,
-					contextIsolation: true,
-					devTools: false
-				},
-				show: false,
-				frame: false
-			});
-			// no window menu
-			modal.setMenu(null);
-			remote.enable(modal.webContents);
-			const modalUrl = "file://" + __dirname + "/assets/modal/modal.html";
-			// modal.loadURL(`${modalUrl}?devMode=${devMode}&muted=${muted}&sensitive=${sensitive}`);
-			modal.loadURL(`${CONFIG_WINDOW_WEBPACK_ENTRY}?devMode=${devMode}&muted=${muted}&sensitive=${sensitive}&voiceOver=${voiceOver}`);
-			// modal.loadURL(CONFIG_WINDOW_WEBPACK_ENTRY, {});
-			// modal.webContents.openDevTools(); // temp
-
-			modal.once('ready-to-show', () => {
-				modal.show();
-			});
-
-			modal.on('close', () => {
-				app.quit();
-			});
-
-			return;
-		}
-		/* PREVIEW */
-		// TODO: https://support.microsoft.com/en-ca/help/182383/info-screen-saver-command-line-arguments
-		// The /p option tells us to display the screen saver in the tiny preview window in the Screen Saver Settings dialog.
-		else if ( process.argv[argNum].match(/^\/p/)) { // preview
-			app.quit();
-			// try child window
-			// not works :( showing new window under the properties panel, should be inside image of monitor
-			// const preview = new ScreenSaverGallery(true, devMode, store);
-			// preview.init();
-			return;
-		/* SCREENSAVER */
-		} else {
-			const execPath = path.resolve(process.execPath);
-			registerScreenSaver(execPath, true);
-			const ssg = new ScreenSaverGallery(false, devMode, store);
-			ssg.init();
-		}
-
-		// The /a option change password, modal to window.
-		/* else if (process.argv[1] === "/a")
-		{
-			app.quit();
-			return;
-		} */
-
-		// The /S option is passed when the user chooses Configure from the .scr file context menu (although we don't see this in practice).
-		// The /c:# option is passed when the user clicks Settings... in the Screen Saver Settings dialog.
-		
-	}
 });
 
-function registerScreenSaver(scrPath: string, add: boolean): void {
-	if (add) {
-		const hkey_user = "HKEY_CURRENT_USER\\Control\ Panel\\Desktop";
-		// set for current user (setup every time screensavergallery runs)
-		try {
-			ChildProcess.execSync(`REG ADD "${hkey_user}" /v SCRNSAVE.EXE /t REG_SZ /d "${scrPath}" /f`);
-			ChildProcess.execSync(`REG ADD "${hkey_user}" /v ScreenSaveActive /t REG_SZ /d 1 /f`);
-			// set once while install ChildProcess.execSync(`REG ADD "${hkey_user}" /v ScreenSaveTimeOut /t REG_SZ /d 360 /f`); // 360 = 60 * 6 = 6min	
-		} catch(e) {
-			console.log('error', e);
-		}
-			
-	}
+function initTray() {
+    // tray
+    tray = new Tray(`${__dirname}/assets/iconset/ssg-icon-32.png`);
+    const trayMenu: Menu = Menu.buildFromTemplate([
+        {
+            label: '🦑️ Options',
+            click: () => { showConfigModal(); }
+        },
+        {
+            label: '🐣️ Preview',
+            click: () => { startScreenSaver(); }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            role: 'quit'
+        }
+    ]);
+    tray.setContextMenu(trayMenu);
+}
+
+function showConfigModal() {
+    const modal = new BrowserWindow({
+        width: store.devMode ? 1400 : 700, 
+        height: 512, 
+        webPreferences: {
+            sandbox: false,
+            nodeIntegration: true,
+            preload: CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY, // `${__dirname}/assets/modal/modal.js`,
+            contextIsolation: true,
+            devTools: store.devMode
+        },
+        show: false,
+        frame: true,
+        roundedCorners: true
+    });
+    modal.setIcon(`${__dirname}/assets/iconset/ssg-icon-128.png`)
+    // no window menu
+    modal.setMenu(null);
+    remote.enable(modal.webContents);
+    // modal.loadURL(`${modalUrl}?devMode=${devMode}&muted=${muted}&sensitive=${sensitive}`);
+    const qureyParams = `devMode=${store.devMode}&muted=${store.muted}&sensitive=${store.sensitive}&voiceOver=${store.voiceOver}&startSSG=${store.startSSG}&displayOff=${store.displayOff}&requirePass=${store.requirePass}&runOnBattery=${store.runOnBattery}`;
+    modal.loadURL(`${CONFIG_WINDOW_WEBPACK_ENTRY}?${qureyParams}`);
+    // modal.loadURL(CONFIG_WINDOW_WEBPACK_ENTRY, {});
+    if (store.devMode) modal.webContents.openDevTools(); // temp
+
+    modal.once('ready-to-show', () => {
+        modal.show();
+    });
+
+    modal.on('close', () => {
+        // app.quit();
+    });
+}
+
+function startScreenSaver() {
+	const ssg = new ScreenSaverGallery(false, store.devMode, store);
+	ssg.init();
 }

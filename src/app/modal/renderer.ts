62b00f2d787f82e90ab7650542da7f22143731dd
Tomáš <info@metazoa.org>
2024-07-17 00:20:17 +0200

    add linux screensaver control options

--- src/app/modal/renderer.ts
+++ src/app/modal/renderer.ts
@@ -7,6 +7,61 @@ declare global {
     }
 }
 
+export class ConfigModal {
+    constructor(){}
+
+    muteChanged(event: any) {
+        const { checked } = event.target;
+        // console.log('muteChanged checked?', checked);
+        window.action.muted(checked);
+    }
+
+    sensitiveChanged(event: any) {
+        const { checked } = event.target;
+        window.action.sensitive(checked);
+    }
+
+    debugChanged(event: any) {
+        const { checked } = event.target;
+        window.action.devMode(checked);
+    }
+    
+    voiceOverChanged(event: any) {
+        const { checked } = event.target;
+        window.action.voiceOver(checked);
+    }
+
+    startSSGChanged(event: any) {
+        const { value } = event.target;
+        window.action.startSSG(value);
+    }
+
+    displayOffChanged(event: any) {
+        const { value } = event.target;
+        window.action.displayOff(value);
+    }
+
+    requirePassChanged(event: any) {
+        const { value } = event.target;
+        window.action.requirePass(value);
+    }
+
+    runOnBatteryChanged(event: any) {
+        const { checked } = event.target;
+        window.action.runOnBattery(checked);
+    }
+
+    
+    openLink(link: string) {
+        window.action.openLink(link);
+    }
+    
+    closeWindow() {
+        window.close();
+        return true;
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -14,7 +69,11 @@ document.addEventListener('DOMContentLoaded', () => {
         devMode: searchParams.get('devMode') === 'true',
         sensitive: searchParams.get('sensitive') === 'true',
         muted: searchParams.get('muted') === 'true',
-        voiceOver: searchParams.get('voiceOver') === 'true'
+        voiceOver: searchParams.get('voiceOver') === 'true',
+        startSSG: searchParams.get('startSSG'),
+        displayOff: searchParams.get('displayOff'),
+        requirePass: searchParams.get('requirePass'),
+        runOnBattery: searchParams.get('runOnBattery') === 'true'
     }
     const links = {
         support: 'https://screensaver.gallery/support-us?app=win',
@@ -29,65 +88,56 @@ document.addEventListener('DOMContentLoaded', () => {
     if (debugCheckboxElm) debugCheckboxElm.checked = config.devMode;
     const voiceOverCheckboxElm: HTMLInputElement | null = document.querySelector('#low-vision-checkbox');
     if (voiceOverCheckboxElm) voiceOverCheckboxElm.checked = config.voiceOver;
-    
+    const startSSGElm: HTMLInputElement | null = document.querySelector('#start-ssg');
+    if (startSSGElm) startSSGElm.value = config.startSSG ? config.startSSG : '0';
+    const displayOffElm: HTMLInputElement | null = document.querySelector('#display-off');
+    if (displayOffElm) displayOffElm.value = config.displayOff ? config.displayOff : '0';
+    const requirePassElm: HTMLInputElement | null = document.querySelector('#require-pass');
+    if (requirePassElm) requirePassElm.value = config.requirePass ? config.requirePass : '0';
+    const runOnBatteryElm: HTMLInputElement | null = document.querySelector('#run-on-battery');
+    if (runOnBatteryElm) runOnBatteryElm.checked = config.runOnBattery;
+
     const supportLinkElm: HTMLLinkElement | null = document.querySelector('#support');
     const contactLinkElm: HTMLLinkElement | null = document.querySelector('#contact');
     const callLinkElm: HTMLLinkElement | null = document.querySelector('#call');
     const closeElm: HTMLLinkElement | null = document.querySelector('#close');
 
+    const modal = new ConfigModal();
+
     supportLinkElm?.addEventListener('click', () => {
-        openLink(links.support);
+        modal.openLink(links.support);
     });
     contactLinkElm?.addEventListener('click', () => {
-        openLink(links.contact);
+        modal.openLink(links.contact);
     });
     callLinkElm?.addEventListener('click', () => {
-        openLink(links.call);
+        modal.openLink(links.call);
     });
     muteCheckboxElm?.addEventListener('change', (event: any) => {
-        muteChanged(event);
+        modal.muteChanged(event);
     });
     sensitiveCheckboxElm?.addEventListener('change', (event: any) => {
-        sensitiveChanged(event);
+        modal.sensitiveChanged(event);
     });
     voiceOverCheckboxElm?.addEventListener('change', (event: any) => {
-        voiceOverChanged(event);
+        modal.voiceOverChanged(event);
     });
     debugCheckboxElm?.addEventListener('change', (event: any) => {
-        debugChanged(event);
+        modal.debugChanged(event);
+    });
+    startSSGElm?.addEventListener('change', (event: any) => {
+        modal.startSSGChanged(event);
+    });
+    displayOffElm?.addEventListener('change', (event: any) => {
+        modal.displayOffChanged(event);
+    });
+    requirePassElm?.addEventListener('change', (event: any) => {
+        modal.requirePassChanged(event);
+    });
+    runOnBatteryElm?.addEventListener('change', (event: any) => {
+        modal.runOnBatteryChanged(event);
     });
     closeElm?.addEventListener('click', () => {
-        closeWindow();
+        modal.closeWindow();
     });
 });
-
-
-export function muteChanged(event: any) {
-    const { checked } = event.target;
-    // console.log('muteChanged checked?', checked);
-    window.action.muted(checked);
-}
-
-export function sensitiveChanged(event: any) {
-    const { checked } = event.target;
-    window.action.sensitive(checked);
-}
-
-export function debugChanged(event: any) {
-    const { checked } = event.target;
-    window.action.devMode(checked);
-}
-
-export function voiceOverChanged(event: any) {
-    const { checked } = event.target;
-    window.action.voiceOver(checked);
-}
-
-export function openLink(link: string) {
-    window.action.openLink(link);
-}
-
-export function closeWindow() {
-    window.close();
-    return true;
-}

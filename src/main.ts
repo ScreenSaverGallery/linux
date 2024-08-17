0f7604992b7e754a87110267e0a3a4d7a9de9576
Tomáš <info@metazoa.org>
2024-08-17 14:05:17 +0200

    remove electron-squirrel-startup

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { Store } from './app/store';
 import { ScreenSaverGallery } from './app/screen-saver-gallery';
 import { v4 as uuidv4 } from 'uuid';
 import { spawn } from 'child_process';
-import * as path from 'path';
-import * as ChildProcess from 'child_process';
 import AutoLaunch from 'auto-launch';
 // rxjs
 import { Subscription } from 'rxjs';
@@ -17,9 +15,9 @@ declare const CONFIG_WINDOW_WEBPACK_ENTRY: string;
 declare const CONFIG_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-	app.quit();
-}
+// if (require('electron-squirrel-startup')) {
+// 	app.quit();
+// }
 // initialize remote
 remote.initialize();
 
